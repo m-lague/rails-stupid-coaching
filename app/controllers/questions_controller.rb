830f023c37c2abe7622432f73ce27801bd139b61
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-07 12:58:34 +0200

    add bootstrap

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,25 @@
 class QuestionsController < ApplicationController
+
   def answer
     @question = params["question"]
+    coach_answer_enhanced(@question)
   end
 
-  def self.coach_answer(your_message)
+  def coach_answer(your_message)
     if your_message.downcase == "i am going to work right now!"
-      ""
+      @answer = ""
     elsif your_message.end_with?("?")
-      "Silly question, get dressed and go to work!"
+     @answer = "Silly question, get dressed and go to work!"
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 
-  def self.coach_answer_enhanced(your_message)
+  def coach_answer_enhanced(your_message)
     answer = coach_answer(your_message)
     if answer != ""
       if your_message.upcase == your_message
-        "I can feel your motivation! #{answer}"
+        @answer = "I can feel your motivation! #{answer}"
       else
         answer
       end
